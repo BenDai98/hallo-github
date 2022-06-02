515d12bb554ac9852f4928ed82e4cdf73b043b0e
BenDai98 <benjamin.daiss055@googlemail.com>
2022-06-02 21:51:39 +0200

    Added some function

--- src/firstTestPackge/TestApplication.java
+++ src/firstTestPackge/TestApplication.java
@@ -3,5 +3,8 @@ package firstTestPackge;
 public class TestApplication {
     public static void main(String[] args) {
         System.out.println("Hello Java");
+        for(int i = 0; i<5;i++){
+            System.out.println(i);
+        }
     }
 }
